37e0c7f4d9066a94ee2a4dab0971bc31c0b5575b
Jeremy Blanchard <auzigog@gmail.com>
2011-12-07 01:48:37 -0800

    Add basic functionality for jeremys route

--- nextemx/apps/mainsite/views.py
+++ nextemx/apps/mainsite/views.py
@@ -2,6 +2,7 @@ from django import http
 from django import template
 from django.conf import settings
 from django.views.generic import TemplateView
+from nextemx.models import NextEmX
 from routeshout import RouteShoutAPI
 
 def error500(request, template_name='500.html'):
@@ -24,7 +25,9 @@ class TestView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(TestView, self).get_context_data(**kwargs)
-        api = RouteShoutAPI(settings.ROUTESHOUT_API_KEY)
-        a = api.agencies_getList()
-        r = api.routes_getList('ltd')
+        n = NextEmX()
+        jdata = n.jeremy()
+
+        context.update(jdata)
+
         return context
--- nextemx/apps/nextemx/models.py
+++ nextemx/apps/nextemx/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
+from routeshout import RouteShoutAPI
 
 # Create your models here.
+
+class NextEmX(object):
+    LTD = 'ltd'
+    api = RouteShoutAPI(settings.ROUTESHOUT_API_KEY)
+
+    def __init__(self):
+        pass
+
+    def jeremy(self):
+#        routes = self.api.routes_getList(self.LTD)
+        emx_route = 4656
+#        stops = self.api.stops_getList(self.LTD, emx_route)
+        walnut_stop = 1651
+        eugene_station_stop = 1310
+
+        walnut_times = self.api.stops_getTimes(self.LTD, walnut_stop)
+        eugene_station_times = self.api.stops_getTimes(self.LTD, eugene_station_stop)
+
+        result = {
+            'walnut': walnut_times,
+            'eugene':eugene_station_times,
+        }
+        return result

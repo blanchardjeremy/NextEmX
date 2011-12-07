1f0442464c413c88c6f9c1f6d2970f55434744b8
Jeremy Blanchard <auzigog@gmail.com>
2011-12-07 04:01:04 -0800

    Refactor so settings can come from environment or local python

--- nextemx/apps/mainsite/settings.py
+++ nextemx/apps/mainsite/settings.py
@@ -105,8 +105,66 @@ LOGGING = {
 
 
 
+#### Local settings
+
 # try to import settings_local if present
 try:
     from settings_local import *
 except ImportError as e:
     pass
+
+
+def get_local_config(variable_name, default=None):
+    """
+    Allows for configuration to happen with environmental variables (for Heroku) or python variables (for PyCharm)
+    """
+    py_var = locals().get(variable_name)
+    if py_var:
+        return py_var
+    os_var = os.environ.get(variable_name)
+    if os_var:
+        return os_var
+    return default
+
+
+DEBUG = get_local_config('DJANGO_DEBUG', True)
+TEMPLATE_DEBUG = DEBUG
+
+TIME_ZONE = 'America/Los_Angeles'
+LANGUAGE_CODE = 'en-us'
+
+ROUTESHOUT_API_KEY = get_local_config('ROUTESHOUT_API_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': get_local_config('DB_NAME', ''),                      # Or path to database file if using sqlite3.
+        'USER': get_local_config('DB_USER', ''),
+        'PASSWORD': get_local_config('DB_PASS', ''),
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': '',
+        'TIMEOUT': 300,
+        'KEY_PREFIX': '',
+        'VERSION': 1,
+    }
+}
+
+
+if DEBUG:
+    # Example of how to include debug toolbar in local_settings
+    MIDDLEWARE_CLASSES.insert(0,'debug_toolbar.middleware.DebugToolbarMiddleware')
+    INSTALLED_APPS.append('debug_toolbar')
+    INTERNAL_IPS = (
+       '127.0.0.1',
+    )
+    JINGO_EXCLUDE_APPS.append('debug_toolbar')
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False
+    }

28f2cc3926d831b03490979414bf8dee225dc739
Jeremy Blanchard <auzigog@gmail.com>
2011-12-08 01:09:24 -0800

    Change caching key approach

--- nextemx/apps/nextemx/models.py
+++ nextemx/apps/nextemx/models.py
@@ -13,9 +13,11 @@ def cacheable(timeout=None, key=None):
     def wrap(f):
 
         def inner(self, *args, **kwargs):
-            cache_key = f.__name__
-            if args and kwargs:
-                cache_key = cache_key+':'+md5(':'.join(force_unicode(v) for v in (list(args) + kwargs.items()))).hexdigest()
+            key_parts = [self.__class__.__name__, f.__name__]
+            if args or kwargs:
+                key_parts.append(md5(':'.join(force_unicode(v) for v in (list(args) + kwargs.items()))).hexdigest())
+
+            cache_key = ':'.join(key_parts)
 
             chunk = cache.get(cache_key)
             if chunk is None:
@@ -32,7 +34,6 @@ def cacheable(timeout=None, key=None):
 
 class NextEmX(object):
     LTD = 'ltd'
-    CACHE_TIMEOUT = 60
     api = RouteShoutAPI(settings.ROUTESHOUT_API_KEY)
 
     def __init__(self):
@@ -40,29 +41,20 @@ class NextEmX(object):
 
     def jeremy(self):
 #        routes = self.api.routes_getList(self.LTD)
-        emx_route = 4656
+#        emx_route = 4656
 #        stops = self.api.stops_getList(self.LTD, emx_route)
 
-        walnut_times = self.jeremy_walnut()
-        eugene_station_times = self.jeremy_eugene()
+        walnut_stop = 1651
+        eugene_station_stop = 1310
+        walnut_times = self.get_stop_times(walnut_stop)
+        eugene_station_times = self.get_stop_times(eugene_station_stop)
 
         result = {
             'walnut': walnut_times,
             'eugene': eugene_station_times,
         }
         return result
-    
-    @cacheable()
-    def jeremy_walnut(self):
-        walnut_stop = 1651
-        return self.api.stops_getTimes(self.LTD, walnut_stop)
 
-    def jeremy_eugene(self):
-        eugene_station_stop = 1310
-        key = 'eugene'
-        if cache.get(key):
-            return cache.get(key)
-        else:
-            val = self.api.stops_getTimes(self.LTD, eugene_station_stop)
-            cache.set(key, val, self.CACHE_TIMEOUT)
-            return val
+    @cacheable()
+    def get_stop_times(self, id):
+        return self.api.stops_getTimes(self.LTD, id)

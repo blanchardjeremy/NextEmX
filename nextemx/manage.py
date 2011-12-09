1e5e527b4d4639d07b96d3c9211d13e6bef835de
Jeremy Blanchard <auzigog@gmail.com>
2011-12-08 18:23:36 -0800

    Change manage.py

--- nextemx/manage.py
+++ nextemx/manage.py
@@ -12,10 +12,11 @@ if APPS_DIR not in sys.path:
     sys.path.insert(0, APPS_DIR)
 
 try:
-    import settings
+    from mainsite import settings
 except ImportError:
-    sys.stderr.write("Error: Can't load python module 'mainsite.settings' from the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+    pass
+#    sys.stderr.write("Error: Can't load python module 'mainsite.settings' from the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+#    sys.exit(1)
 
 
 if __name__ == "__main__":

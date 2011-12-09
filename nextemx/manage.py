f5dec986e2cb3cdcd6de1d3bc9ff1bf92886e728
Jeremy Blanchard <auzigog@gmail.com>
2011-12-08 18:32:13 -0800

    Revert to default django project structure

--- nextemx/manage.py
+++ nextemx/manage.py
@@ -12,11 +12,13 @@ if APPS_DIR not in sys.path:
     sys.path.insert(0, APPS_DIR)
 
 try:
-    from mainsite import settings
+    imp.find_module('settings') # Assumed to be in the same directory.
 except ImportError:
-    pass
-#    sys.stderr.write("Error: Can't load python module 'mainsite.settings' from the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-#    sys.exit(1)
+    import sys
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
+    sys.exit(1)
+
+import settings
 
 
 if __name__ == "__main__":
